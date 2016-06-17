50c8897c12bb6c2caabc94222ed57d16d92e64d2
Karl Girthofer <kgirthofer@delphicdigital.com>
2016-06-17 15:15:05 -0400

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kgirthofer@delphicdigital.com'
 license 'All rights reserved'
 description 'Installs/Configures delphic_aws'
 long_description 'Installs/Configures delphic_aws'
-version '0.0.1'
+version '0.0.2'
 
 ## depends on another cookbook, or series of cookbooks?
 ## depends 'some_cookbook'
